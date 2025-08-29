f9ac93ab845a8b97391a7898ff4c76b5122c0d84
Dario <dar.moralesa@duocuc.cl>
2025-08-29 14:14:54 -0400

    Creacion de los elementos navBarppal navBar y carrousel

---
+++ Entrega 1/js/components/headerPpal.js
@@ -0,0 +1,19 @@
+document.getElementById("headerPpal").innerHTML =  `
+    <header class="headerPpal">
+            <nav class="navbar1">
+                <a class="navbar-brand" href="index.html" id="logo">
+                    <img src="assets/icons/icono.png" alt="icono" width="60" height="auto"
+                        class="d-inline-block align-text-center" />
+                    <strong class="logoName">Level-Up</strong>
+                </a>
+                <ul class="navBarButtons">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="pages/login.html">Ingresar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="pages/signin.html">Registrarse</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+`;
---
+++ Entrega 1/js/components/navbarProductos.js
@@ -0,0 +1,42 @@
+document.getElementById("navbarProductos").innerHTML =  `
+    <nav class="navbar navbar-expand" data-bs-theme="dark" id="navbar1">
+        <div class="container-fluid justify-content-center">
+            <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Zona Gamer
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="pages/productos/zonaGamer/consolas.html">Consolas</a></li>
+                        <li><a class="dropdown-item" href="pages/productos/zonaGamer/computadores.html">Computadores Gamer</a></li>
+                        <li><a class="dropdown-item" href="pages/productos/zonaGamer/mouse.html">Mouse</a></li>
+                        <li><a class="dropdown-item" href="pages/productos/zonaGamer/mousepad.html">MousePad</a></li>
+                        <li><a class="dropdown-item" href="pages/productos/zonaGamer/sillas.html">Sillas Gamer</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Ropa y Personalizados
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="pages/productos/ropa/poleras.html">Poleras Personalizadas</a></li>
+                        <li><a class="dropdown-item" href="pages/productos/ropa/polerones.html">Polerones Gamer Personalizados</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Juegos y Accesorios
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="pages/productos/Juegos/accesorios.html">Accesorios</a></li>
+                        <li><a class="dropdown-item" href="pages/productos/Juegos/juegosMesa.html">Juegos de Mesa</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+    </nav>
+`;
